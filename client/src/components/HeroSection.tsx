6b1d6a6bef31b139cf04afee344c5f63a351a575
Abhinav Gupta <gupta380189@gmail.com>
2024-08-20 20:48:00 +0530

    ok

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ function HeroSection() {
     return (
       <>
         <div className="w-full md:flex p-6 md:p-12 bg-black">
-          <div className="w-full md:w-[50%] h-[300px] md:h-[500px] p-4 md:p-8 md:px-24 bg-fixed bg-cover bg-center rounded-2xl border-2" style={{ backgroundImage: "url('./public/backgroung.jpg')" }}>
+          <div className="w-full md:w-[50%] h-[300px] md:h-[500px] p-4 md:p-8 md:px-24 bg-fixed bg-cover bg-center rounded-2xl border-2" style={{ backgroundImage: "url(' backgroung.jpg')" }}>
             <div className="w-full h-[55%] text-white text-2xl md:text-5xl font-extrabold tracking-wider leading-normal ">
               WANT TO BUILD <br /> A HACKATHON <br /> TEAM ?
             </div>
@@ -18,13 +18,13 @@ function HeroSection() {
           
           <div className="w-full md:w-[50%] h-[300px] md:h-[500px] px-[60px] md:px-[90px] bg-black">
             <div className="w-full h-[50%] py-4 mt-2 md:mt-0 flex justify-center md:translate-x-[70px]">
-              <img src="./public/hat.jpg" className="h-full" />
+              <img src=" hat.jpg" className="h-full" />
             </div>
             <div className="w-full h-[30%] md:pt-6 pt-2  md:mt-0 text-xl md:text-5xl  font-extrabold flex justify-center md:justify-end text-white leading-tight">
               CODECAP IS HERE <br /> TO HELP!
             </div>
             <div className="w-full h-[20%] flex justify-center pt-0 md:translate-x-[70px]">
-              <img src="./public/arrow-removebg-preview.png" className="h-full" alt="" />
+              <img src=" arrow-removebg-preview.png" className="h-full" alt="" />
             </div>
           </div>
         </div>
--- client/src/components/WhatWeOffer.tsx
+++ client/src/components/WhatWeOffer.tsx
@@ -16,14 +16,14 @@ const WhatWeOffer: React.FC = () => {
       <h1 className="text-3xl md:text-5xl font-bold mb-6 md:mb-8">What We Offer</h1>
       <div className="flex flex-col md:flex-row justify-center space-y-6 md:space-y-0 md:space-x-8">
         <div className="border-2 border-yellow-400 p-4 w-full md:w-[600px] bg-gray-900">
-          <img src="./public/Hackupdate.jpg" alt="Hackathon Updates" className="w-full h-auto mb-4" />
+          <img src=" Hackupdate.jpg" alt="Hackathon Updates" className="w-full h-auto mb-4" />
           <h2 className="text-yellow-400 text-xl md:text-2xl mb-2">Hackathon Updates</h2>
           <button onClick={handlehack} className="bg-black text-xl text-white border-2 border-white px-4 py-2 hover:bg-yellow-400 hover:text-black hover:font-bold">
             Check In
           </button>
         </div>
         <div className="border-2 border-yellow-400 p-4 w-full md:w-[600px] bg-gray-900">
-          <img src="./public/buildteam.jpg" alt="Build a Team" className="w-full h-auto mb-4" />
+          <img src=" buildteam.jpg" alt="Build a Team" className="w-full h-auto mb-4" />
           <h2 className="text-yellow-400 text-xl md:text-2xl mb-2">Build a Team</h2>
           <button onClick={handleteam} className="bg-black text-white border-2 border-white px-4 py-2 hover:bg-yellow-400 text-xl hover:font-bold hover:text-black">
             Get Started
